c4c3211aa601654f58107fde3ed1d7081966f89f
rezaalfah <151593508+rezaalfah@users.noreply.github.com>
2024-03-26 10:16:59 +0700

    Add files via upload

---
+++ PTS2024.php
@@ -0,0 +1,58 @@
+<html lang="en">
+  <head>
+    <title>Belajar HTML #01</title>
+  </head>
+  <body>
+    <h1><p><font color="blue">hi ini tugas reza</font></p></h1> 
+    <table border="1px">
+        <tr>
+          <th bgcolor="blue" rowspan="2">no</th>
+          <th bgcolor="blue" rowspan="2">nama</th>
+          <th bgcolor="blue" rowspan="2">alamat</th>
+          <th bgcolor="blue"rowspan="2">jenis kelamin</th>
+          <th bgcolor="green" colspan="3">data nilai</th>
+</tr>
+        <tr>
+          <td bgcolor="yellow">IPA</td> 
+          <td bgcolor="yellow">IPS</td>
+          <td bgcolor="yellow">MTK</td>
+</tr>
+        <tr>
+        <td>1</td>
+        <td>IBNU</td>
+        <td>JAKARTA</td>
+        <td>LAKI LAKI</td>
+        <td>80</td>
+        <td>88</td>
+        <td>80</td>
+</tr>
+        <tr>
+          <td>2</td>
+          <td>SARI</td>
+          <td>BOGOR</td>
+          <td>PEREMPUAN</td>
+          <td>85</td>
+          <td>88</td>
+          <td>90</td>
+</tr>
+        <tr>
+          <td>3</td>
+          <td>SUKIMAN</td>
+          <td>BEKASI</td>
+          <td>LAKI LAKI</td>
+          <td>80</td>
+          <td>83</td>
+          <td>88</td>
+</tr>
+        <tr>
+          <td>4</td>
+          <td>TARMI</td>
+          <td>INDRAMAYU</td>
+          <td>PEREMPUAN</td>
+          <td>75</td>
+          <td>80</td>
+          <td>78</td>
+</tr>
+
+
+       
